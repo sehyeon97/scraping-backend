ce7f936a9405aedb6fa95deac7f0e59f4debfb5d
sehyeon <sehyeonoh97@gmail.com>
2023-02-20 20:48:18 -0800

    recursion 101

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ import express from "express";
 import puppeteer from "puppeteer";
 
 const app = express();
+// #tbArticle > div.articleFile > a
+// #powerbbsContent > div:nth-child(5) > div:nth-child(15) > a
+// #powerbbsContent > div:nth-child(6) > span > a
+// #gSeriesView > div.articleFile
+
+// 
 
 const MY_AGENT = 'Chrome/88.0.4298.0';
 const MAIN_PAGE = 'https://www.inven.co.kr/search/';
@@ -16,6 +22,17 @@ const SEARCH_GUNNER_MALE = '상시공유 헌터';
 const SEARCH_GUNNER_FEMALE = '상시공유 건슬링어';
 const SEARCH_ASSASSIN = '상시공유 데런';
 
+const MARTIAL_ARTIST_PAGE = 'https://www.inven.co.kr/search/webzine/article/%EC%83%81%EC%8B%9C%EA%B3%B5%EC%9C%A0%20%EC%95%A0%EB%8B%88%EC%B8%A0/1'
+const TEST = '#searchBody > div.commu-wrap > section > article > section > div.commu-body.pcMain > div > div.section_box.noboard > div.section_body > ul';
+const IMAGE_BODY = '#powerbbsContent';
+// #powerbbsContent > div:nth-child(5) > div:nth-child(6) > img:nth-child(1)
+// #powerbbsContent > div:nth-child(24) > img
+
+// #powerbbsContent > div:nth-child(5) > div:nth-child(6)
+// #powerbbsContent > div:nth-child(5) > div:nth-child(11)
+
+// #powerbbsContent > div:nth-child(21) > img:nth-child(1)
+
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
@@ -26,27 +43,79 @@ app.post("/post", (req, res) => {
 });
 
 // launch puppeteer's headless browser
-async function run() {
-    const browser = await puppeteer.launch({
-        headless: false
-    });
+const browser = await puppeteer.launch({
+    headless: false
+});
+
+async function getLinksToPosts() {
     const page = await browser.newPage();
     await page.setUserAgent(MY_AGENT);
-    await page.goto(MAIN_PAGE);
+    await page.setViewport({
+        width: 1200,
+        height: 800
+    })
 
-    await page.click(SEARCH_BAR);
-    await page.keyboard.type(SEARCH_MARTIAL_ARTIST_PRESETS, {delay: 1000});
-    await page.click(SEARCH_BUTTON);
-    await page.waitForNavigation();
+    // martial artist search result page
+    await page.goto(MARTIAL_ARTIST_PAGE, {waitUntil: "load"});
+    // wait for the body containing all the links to be rendered
+    await page.waitForSelector(TEST);
+
+    let urls = await page.$$eval(TEST + ' > li', links => {
+        // Extract the links from the data
+        links = links.map(element => element.querySelector('h1 > a').href)
+        return links;
+    });
+
+    return urls;
     
-    await page.click(CLICK_TO_SEE_MORE);
-    await page.waitForNavigation();
-
-    await page.screenshot({ path: 'screenshots/test.png' });
-    browser.close();
+    // await page.screenshot({ path: 'screenshots/test.png' });
+    // browser.close();
 }
 
-run();
+function recursion(domElement) {
+    if (domElement.startsWith('img')) {
+        return domElement.src;
+    } else if (domElement.childElementCount() == 0) {
+        return null;
+    }
+
+    return recursion(domElement.nextChild())
+}
+
+async function getImagesFromLinks(links) {
+    for (let link in links) {
+        let newPage = await browser.newPage();
+        await newPage.setUserAgent(MY_AGENT);
+        await newPage.setViewport({
+            width: 1200,
+            height: 800
+        })
+        console.log(links[link]);
+        // wait until there is no more than zero incoming traffic
+        await newPage.goto(links[link], {waitUntil: "networkidle0"});
+        
+
+
+        console.log("FIRST HERE");
+        await newPage.waitForSelector(IMAGE_BODY, {timeout: 0});
+        console.log("SECOND HERE");
+        let imageContent = document.querySelectorAll(IMAGE_BODY);
+        let images = [];
+        for (let content in imageContent) {
+            let data = recursion(content);
+            if (data != null) {
+                images.push(data);
+            }
+        }
+        
+        console.log("For " + urls[link] + " printing all images:")
+        console.log(images);
+    }
+}
+
+const links = await getLinksToPosts();
+console.log(links);
+await getImagesFromLinks(links);
 
 const PORT = process.env.PORT || 8080;
 
